8270578aefa191af315f1d29b92cd3d3c58d8296
María Rosa González <gomaroluz@gmail.com>
2022-10-09 16:31:43 -0300

    ejercicio dia uno ok

---
+++ codigo.js
@@ -0,0 +1,12 @@
+let numeroUno;
+let numeroDos;
+const titulo;
+const contenedor;
+const resultado;
+let booleanaUno;
+let booleanaDos;
+let alumno;
+alumno = 'María Rosa Luz González'
+let comision;
+comision = 22563
+
