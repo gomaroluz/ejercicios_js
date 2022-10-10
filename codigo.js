547c2b04140c53f77719a9aa7134ac20bed435f4
María Rosa González <gomaroluz@gmail.com>
2022-10-09 22:31:13 -0300

    ejercicio dia 2 ok

--- codigo.js
+++ codigo.js
@@ -1,12 +1,26 @@
 let numeroUno;
 let numeroDos;
-const titulo;
-const contenedor;
-const resultado;
 let booleanaUno;
 let booleanaDos;
 let alumno;
 alumno = 'María Rosa Luz González'
 let comision;
 comision = 22563
+
+
+numeroUno = 53
+numeroDos = 28
+numeroUno *= 2
+numeroUno +=5
+numeroUno *= 50
+numeroUno += 28
+numeroUno -= 365
+numeroUno += 115
+
+
+
+const edades = numeroUno;
+const resultado = document.querySelector(".resultado");
+resultado.textContent = edades;
+
 
